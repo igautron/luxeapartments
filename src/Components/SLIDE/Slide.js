c2f0e006adf3a11e8ad0e965758ebd1a2c8e6c34
aniqdev <aniq.dev@gmail.com>
2020-12-15 19:24:17 +0200

    slider

--- src/Components/SLIDE/Slide.js
+++ src/Components/SLIDE/Slide.js
@@ -8,9 +8,11 @@ import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDB
 
 import './../../Image/mainphoto.png'
 
+import imageSrc from '../../Image/StandardLogoFiles/OriginalonTransparent.png'
 
 class Slide extends React.Component  {
     render() {
+        // let src = require('../../Image/StandardLogoFiles/OriginalonTransparent.png')
         return (
             <div>
                 <div className='slide-images'>
@@ -24,39 +26,49 @@ class Slide extends React.Component  {
                       >
                         <MDBCarouselInner>
                             <MDBCarouselItem itemId="1">
-                                <MDBView>
+                                <MDBView className="position-relative">
                                     <img
                                         className="slide-image d-block w-100"
                                         src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg"
                                         alt="First slide"
                                     />
+                                    <div className='slide-info position-absolute'>
+                                        <img src={imageSrc} />
+                                        <h1>111 We offer the best apartments in the most attractive districts of Odessa to ensure your comfort</h1>
+                                        <button className='slide-button py-1 px-3'>MORE</button>
+                                    </div>
                                 </MDBView>
                             </MDBCarouselItem>
                             <MDBCarouselItem itemId="2">
-                                <MDBView>
+                                <MDBView className="position-relative">
                                     <img
                                         className="slide-image d-block w-100"
                                         src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg"
                                         alt="Second slide"
                                     />
+                                    <div className='slide-info position-absolute'>
+                                        <img src={imageSrc} />
+                                        <h1>222 We offer the best apartments in the most attractive districts of Odessa to ensure your comfort</h1>
+                                        <button className='slide-button py-1 px-3'>MORE</button>
+                                    </div>
                                 </MDBView>
                             </MDBCarouselItem>
                             <MDBCarouselItem itemId="3">
-                                <MDBView>
+                                <MDBView className="position-relative">
                                     <img
                                         className="slide-image d-block w-100"
                                         src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg"
                                         alt="Third slide"
                                     />
+                                    <div className='slide-info position-absolute'>
+                                        <img src={imageSrc} />
+                                        <h1>333 We offer the best apartments in the most attractive districts of Odessa to ensure your comfort</h1>
+                                        <button className='slide-button py-1 px-3'>MORE</button>
+                                    </div>
                                 </MDBView>
                             </MDBCarouselItem>
                         </MDBCarouselInner>
                     </MDBCarousel>
-                </div>
-                <div className='slide-info'>
-                    <img src={require('./../../Image/Standard Logo Files/Original on Transparent.png')} />
-                    <h1>We offer the best apartments in the most attractive districts of Odessa to ensure your comfort</h1>
-                    <button className='slide-button py-1 px-3'>MORE</button>
                 </div>
             </div>
         )
